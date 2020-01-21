396dd68efaad332303cfe9d6fdb005a4aadd0eef
31010-16-11 <31010-16-11@exchange.ad.afpanet>
2020-01-21 17:26:40 +0100

    requete axios terminée

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,18 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "../components/SearchBar";
 import VideoList from "./VideoList";
 import axios from "axios";
 
 export default function App() {
-  const movieUrl =
-    "https://api.themoviedb.org/3/movie?sort_by=popularity.desc?api_key=383fad9661a33d6164b48dd1309a05cd";
-  const [movies, setMovies] = useState({});
+  const [movies, setMovies] = useState([]);
 
-  axios.get(movieUrl).then(res => {
-    const movies = res.data;
-    setMovies(movies);
-  });
-  console.log(movies);
+  const API_END_POINT = "https://api.themoviedb.org/3/";
+  const POPULAR_MOVIES_URL = "discover/movie?sort_by=popularity.desc&page=1";
+  const API_KEY = "api_key=383fad9661a33d6164b48dd1309a05cd";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        `${API_END_POINT}${POPULAR_MOVIES_URL}&${API_KEY}`
+      );
+      setMovies(result.data.results);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <SearchBar />
--- src/containers/VideoList.jsx
+++ src/containers/VideoList.jsx
@@ -4,7 +4,9 @@ import VideoListItem from "../components/VideoListItem";
 export default function VideoList({ movies }) {
   return (
     <ul>
-      movies.map( movie => <VideoListItem name={movies.original_title} /> )
+      {movies.map(movie => (
+        <VideoListItem name={movie.original_title} />
+      ))}
     </ul>
   );
 }
