e1dfe7ca2abcbb43084a518958107442dab11af4
31010-16-11 <johan.petrikovsky@gmail.com>
2020-02-10 10:46:52 +0100

    didou

--- src/components/VideoListItem.jsx
+++ src/components/VideoListItem.jsx
@@ -6,6 +6,31 @@ import styled from "@emotion/styled";
 const VideoItem = styled("article")`
   position: relative;
   scroll-snap-align: start;
+
+  .bloc-infos {
+    display: inherit;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    justify-content: center;
+    align-items: center;
+  }
+
+  .bloc-hover {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transition: all 0.3s ease-out;
+  }
+  &:hover .bloc-infos {
+    display: inherit;
+  }
+  &:hover .bloc-hover {
+    background: rgb(32, 37, 47, 0.8);
+  }
 `;
 
 export default function VideoListItem({ movie, callback }) {
@@ -17,10 +42,11 @@ export default function VideoListItem({ movie, callback }) {
 
   return (
     <VideoItem onClick={handleClick}>
+      <div className="bloc-hover"></div>
       <div className="media">
         <img alt={movie.name} src={IMAGE_BASE_URL + movie.poster_path}></img>
       </div>
-      <div className="media-body">
+      <div className="bloc-infos">
         <h3 className="title_list_item">{movie.title}</h3>
         <p className="title_list_item">Price {movie.price}</p>
       </div>
