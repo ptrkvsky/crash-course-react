3877f7a657d5df67332d879ce4dc7d06a0e9fdd8
31010-16-11 <johan.petrikovsky@gmail.com>
2020-02-12 08:37:01 +0100

    build

---
+++ build/precache-manifest.9cef8f282ed29822e7b09c960772185a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a1906d82fdbc4a269cd8b2ce5b263e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34db19d49dd563f2bf98",
+    "url": "/static/css/2.e4728546.chunk.css"
+  },
+  {
+    "revision": "34db19d49dd563f2bf98",
+    "url": "/static/js/2.ec00e11d.chunk.js"
+  },
+  {
+    "revision": "7478ab18f99d6ab577f1b4d3d256caec",
+    "url": "/static/js/2.ec00e11d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2bf5ed544ec0aed6f533",
+    "url": "/static/js/main.4133e5da.chunk.js"
+  },
+  {
+    "revision": "98cf30361ff43120ba19",
+    "url": "/static/js/runtime-main.8196396f.js"
+  }
+]);
